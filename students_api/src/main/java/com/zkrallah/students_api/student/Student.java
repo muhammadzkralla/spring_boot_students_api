3caba3ce1f5433ec6d854f0c08220ee8fc838bbe
Zkrallah <muhammad.heshamyt@gmail.com>
2024-03-22 23:51:33 +0200

    Postgresql Database Connected

--- students_api/src/main/java/com/zkrallah/students_api/student/Student.java
+++ students_api/src/main/java/com/zkrallah/students_api/student/Student.java
@@ -1,8 +1,22 @@
 package com.zkrallah.students_api.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
--- students_api/src/main/java/com/zkrallah/students_api/student/StudentService.java
+++ students_api/src/main/java/com/zkrallah/students_api/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.zkrallah.students_api.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,15 +10,14 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Zkrallah",
-                        "zkrallah@gmail.com",
-                        LocalDate.of(2004, Month.FEBRUARY, 22),
-                        20
-                )
-        );
+        return studentRepository.findAll();
     }
 }
