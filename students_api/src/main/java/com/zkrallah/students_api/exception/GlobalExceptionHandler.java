c89c643c0b7b8e0f0de0544aa8dc41cc983cb646
Zkrallah <muhammad.heshamyt@gmail.com>
2024-04-09 17:33:11 +0200

    GlobalExceptionHandler Implemented.

---
+++ students_api/src/main/java/com/zkrallah/students_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.zkrallah.students_api.exception;
+
+import com.zkrallah.students_api.response.MessageResponse;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<MessageResponse> handleMissingRequestHeader(MissingRequestHeaderException ex) {
+        String errorMessage = "Required request header '" + ex.getHeaderName() + "' is missing";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse(errorMessage));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<MessageResponse> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        String errorMessage = "Required request body is missing: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse(errorMessage));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<MessageResponse> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String errorMessage = "Method argument type mismatch: " + ex.getName() + " should be of type " + Objects.requireNonNull(ex.getRequiredType()).getSimpleName();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse(errorMessage));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<MessageResponse> handleConstraintViolation(ConstraintViolationException ex) {
+        String errorMessage = ex.getMessage();
+        return ResponseEntity.badRequest().body(new MessageResponse(errorMessage));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<MessageResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> ((FieldError) error).getField() + " " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+        String errorMessage = String.join(", ", errorMessages);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse(errorMessage));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ResponseEntity<MessageResponse> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        String errorMessage = "HTTP request method '" + ex.getMethod() + "' is not supported for this endpoint";
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(new MessageResponse(errorMessage));
+    }
+}
